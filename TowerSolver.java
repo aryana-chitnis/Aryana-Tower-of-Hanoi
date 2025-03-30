8a3b832ed77c6fa9da44295171fa80bf31e300d1
aryana-chitnis <ac1000@pleasantonusd.net>
2025-03-30 08:20:12 +0000

    started the overloaded solve method

--- TowerSolver.java
+++ TowerSolver.java
@@ -13,6 +13,10 @@ public class TowerSolver {
     solve(model.getHeight(), 0, 1, 2); // Start solving from tower 0 to tower 2 using tower 1 as auxiliary
 }
 
+    public void solve(int n, int source, int original, int destination) {
+        if (n > 0) {
+            solve(n - 1, source, destination, original); 
+
 // Overloaded recursive solve method
 
     // Create an overloaded solve(...) method
