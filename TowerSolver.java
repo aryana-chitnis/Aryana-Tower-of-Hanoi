d2c6675424bedfab9a09153734bc3f549b9add31
aryana-chitnis <ac1000@pleasantonusd.net>
2025-03-31 04:46:19 +0000

    changed n to count for understanding code

--- TowerSolver.java
+++ TowerSolver.java
@@ -13,11 +13,11 @@ public class TowerSolver {
     solve(model.getHeight(), 0, 1, 2); // Start solving from tower 0 to tower 2 using tower 1 as auxiliary
 }
 
-    public void solve(int n, int source, int original, int destination) {
-        if (n > 0) {
-            solve(n - 1, source, destination, original); 
+    public void solve(int count, int source, int original, int destination) {
+        if (count > 0) {
+            solve(count - 1, source, destination, original); 
             model.move(source, destination);
-            solve(n - 1, original, source, destination);
+            solve(count - 1, original, source, destination);
         }
 
     }
